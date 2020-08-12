792aedeb4db8d2450c7b48d18e28120d57e2b7df
Laura <laura.zhou@yale.edu>
2020-08-12 11:33:40 -0400

    Comments

--- events-interface/__init__.py
+++ events-interface/__init__.py
@@ -1,6 +1,14 @@
 import os, json, functools
 from flask import Flask, request, render_template, jsonify
-import sys, json
+import sys, pathlib
+from pathlib import Path
+from jinja2 import Environment, PackageLoader, select_autoescape
+env = Environment(
+    loader=PackageLoader('events-interface', 'templates'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+file_folder = Path("C:/Users/Laura/seneca/events-interface")
     
 def create_app(test_config=None):
     """Create and configure an instance of the Flask application."""
@@ -25,15 +33,6 @@ def create_app(test_config=None):
     except OSError:
         pass
     
-    #drag and drop example
-    @app.route("/test", methods=("GET", "POST"))
-    def test():
-        data = request.get_json()
-        print(data)
-        with open('data.txt', 'w') as file:
-            file.write(json.dumps(data, indent=4))
-        return render_template("test.html")
-    
     @app.route("/form", methods=("GET", "POST"))
     def form():
         if request.method == 'POST':
@@ -43,6 +42,65 @@ def create_app(test_config=None):
             with open('form.txt', 'w') as file:
                 json.dump(data_formatted, file)
         return render_template("form.html")
+    
+    
+
+    @app.route("/spreadsheet", methods=("GET", "POST"))
+    def spreadsheet():
+        with open(file_folder/'config.txt') as config_file:
+            config_json = json.load(config_file)
+        event_config = {
+                        "types": list(config_json.keys()),
+                        "example_data": list(config_json.values())
+                         }
+        event_config["type_parameters"]={}
+        for type in config_json:
+            event_config["type_parameters"][type]=list(config_json[type].keys())
+        event_config["all_parameters"] = []
+        for event in event_config["type_parameters"]:
+            for parameter in event_config["type_parameters"][event]:
+                event_config["all_parameters"].append(parameter)
+        event_config["all_parameters"] = list(set(event_config["all_parameters"]))
+
+        if request.method == 'POST':
+            dataObj = request.get_json()
+            events = dataObj.keys()
+            columns = json.loads(dataObj[list(events)[0]]["data"])["header"]
+            logic = {}
+            for event in events:
+                subEventsObj = json.loads(dataObj[event]["data"])["body"]
+                eventData ={}
+                eventData["seqType"] = dataObj[event]["sequenceType"]
+                frmtData = []
+                for subEvent in subEventsObj:
+                    subEventType = subEvent[2]
+                    subColumns = config_json[subEventType].keys()
+                    frmtSubEvent = {}
+                    # the first two columns do not contain data
+                    for key in subColumns:
+                        columnIndex = columns.index(key)
+                        frmtSubEvent[key] = subEvent[columnIndex]
+                    frmtData.append(frmtSubEvent)
+                eventData["subEvents"] = frmtData
+                logic[event]=eventData
+            print (json.dumps(logic, indent=2))
+            with open('spreadsheet.json', 'w') as file:
+                json.dump(logic,file, indent=2)
+        return render_template("spreadsheet.html", config1="about", config=event_config)
+    
+    @app.route("/spreadsheet/uploaded", methods=("GET", "POST"))
+    def spreadsheetU():
+        if request.method =='POST':
+                print ('ok')
+        return render_template("spreadsheet.html")
+    
+    @app.route("/spreadsheetM", methods=("GET", "POST"))
+    def spreadsheetM():
+        return render_template("spreadsheet_multiple.html")
+    
+    @app.route("/editor", methods=("GET", "POST"))
+    def editor():
+        return render_template("editor.html")
 
     # make url_for('index') == url_for('blog.index')
     # in another app, you might define a separate main index here with
